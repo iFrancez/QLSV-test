ec2c542ce5f4ad831ab2982019e39478e39aac34
iFrancez <phannguyenthanhphap@gmail.com>
2022-11-11 18:23:09 +0700

    Add project files.

---
+++ QLSV/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace QLSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgvData = new System.Windows.Forms.DataGridView();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgvData
+            // 
+            this.dtgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvData.Location = new System.Drawing.Point(12, 12);
+            this.dtgvData.Name = "dtgvData";
+            this.dtgvData.RowHeadersWidth = 51;
+            this.dtgvData.RowTemplate.Height = 24;
+            this.dtgvData.Size = new System.Drawing.Size(1090, 346);
+            this.dtgvData.TabIndex = 0;
+            // 
+            // btnXem
+            // 
+            this.btnXem.Location = new System.Drawing.Point(83, 374);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(135, 47);
+            this.btnXem.TabIndex = 1;
+            this.btnXem.Text = "Xem";
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(236, 374);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(135, 47);
+            this.btnXoa.TabIndex = 1;
+            this.btnXoa.Text = "Xoá";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(392, 374);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(135, 47);
+            this.btnThem.TabIndex = 1;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(536, 374);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(135, 47);
+            this.btnSua.TabIndex = 1;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtTimKiem.Location = new System.Drawing.Point(699, 376);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(176, 34);
+            this.txtTimKiem.TabIndex = 2;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(896, 374);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(135, 47);
+            this.btnTim.TabIndex = 1;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 446);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnXem);
+            this.Controls.Add(this.dtgvData);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvData;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnTim;
+    }
+}
+
---
+++ QLSV/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            QLSVDataContext db = new QLSVDataContext();
+            //tim kiem theo MaHS
+            dtgvData.DataSource = db.HocSinhs.Where(d => d.MaHS.Equals(txtTimKiem.Text));
+            MessageBox.Show("Tìm thành công", "Tìm");
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            //c1
+            QLSVDataContext db = new QLSVDataContext();
+            dtgvData.DataSource = db.HocSinhs.Select(d => d);
+
+            //c2
+            //using (QLSVDataContext db = new QLSVDataContext())
+            //{
+            //    dtgvData.DataSource = from u in db.HocSinhs
+            //                          select u;
+            //}
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            QLSVDataContext db = new QLSVDataContext();
+            // chon dong dau tien co cot la MaHS
+            string id = dtgvData.SelectedCells[0].OwningRow.Cells["MaHS"].Value.ToString();
+            // lay duy nhat 1 truong
+            HocSinh delete = db.HocSinhs.Where(p => p.MaHS.Equals(id)).SingleOrDefault();
+            //xoa
+            db.HocSinhs.DeleteOnSubmit(delete);
+            //luu lai ( cap nhat lai )
+            db.SubmitChanges();
+            btnXem_Click(sender, e);
+            MessageBox.Show("Xoá thành công", "Xoá");
+        }
+        //them loi
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            //QLSVDataContext db = new QLSVDataContext();
+            //// chon dong dau tien co cot
+            //string id = dtgvData.SelectedCells[0].OwningRow.Cells["MaHS"].Value.ToString();
+            //string name = dtgvData.SelectedCells[0].OwningRow.Cells["TenHS"].Value.ToString();
+            ////DateTime dateofbirth = (DateTime)dtgvData.SelectedCells[0].OwningRow.Cells["NgaySinh"].Value;
+            //string dc = dtgvData.SelectedCells[0].OwningRow.Cells["DiaChi"].Value.ToString();
+            //float dtb = (float)dtgvData.SelectedCells[0].OwningRow.Cells["DTB"].Value;
+            //string ML = dtgvData.SelectedCells[0].OwningRow.Cells["MaLop"].Value.ToString();
+            ////them
+            //HocSinh HS = new HocSinh();
+            //HS.MaHS = id;
+            //HS.TenHS = name;
+            ////khi ngay sinh rong    
+            //if (dtgvData.SelectedCells[0].OwningRow.Cells["NgaySinh"].Value == null) HS.NgaySinh = null;
+            //else HS.NgaySinh = (DateTime)dtgvData.SelectedCells[0].OwningRow.Cells["NgaySinh"].Value;
+            //HS.DiaChi = dc;
+            //HS.DTB = dtb;
+            //HS.MaLop = ML;
+            ////them
+            //db.HocSinhs.InsertOnSubmit(HS);
+            ////luu lai ( cap nhat lai )
+            //db.SubmitChanges();
+            //btnXem_Click(sender, e);
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            QLSVDataContext db = new QLSVDataContext();
+            // chon dong dau tien co cot
+            string id = dtgvData.SelectedCells[0].OwningRow.Cells["MaHS"].Value.ToString();
+            string name = dtgvData.SelectedCells[0].OwningRow.Cells["TenHS"].Value.ToString();
+            //DateTime dateofbirth = (DateTime)dtgvData.SelectedCells[0].OwningRow.Cells["NgaySinh"].Value;
+            string dc = dtgvData.SelectedCells[0].OwningRow.Cells["DiaChi"].Value.ToString();
+            float dtb = (float)dtgvData.SelectedCells[0].OwningRow.Cells["DTB"].Value;
+            string ML = dtgvData.SelectedCells[0].OwningRow.Cells["MaLop"].Value.ToString();
+            //sua
+            HocSinh edit = db.HocSinhs.Where(p => p.MaHS.Equals(id)).SingleOrDefault();
+            edit.TenHS = name;
+            //khi ngay sinh rong    
+            if(dtgvData.SelectedCells[0].OwningRow.Cells["NgaySinh"].Value==null) edit.NgaySinh = null;
+            else edit.NgaySinh = (DateTime)dtgvData.SelectedCells[0].OwningRow.Cells["NgaySinh"].Value;
+
+            edit.DiaChi = dc;
+            edit.DTB = dtb;
+            edit.MaLop = ML;
+            //luu lai ( cap nhat lai )
+            db.SubmitChanges();
+            btnXem_Click(sender, e);
+            MessageBox.Show("Sửa thành công", "Sửa");
+        }
+    }
+}
